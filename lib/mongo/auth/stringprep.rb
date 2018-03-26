6b1145dfeadb45cfb9b1ecbc1cbe54ee63051f58
Saghm Rossi <saghmrossi@gmail.com>
2018-03-26 13:45:36 -0400

    RUBY-1289 Implement SASLPrep

---
+++ lib/mongo/auth/stringprep.rb
@@ -0,0 +1,68 @@
+require 'mongo/auth/stringprep/tables'
+
+module Mongo
+  module Auth
+    module StringPrep
+      extend self
+
+      def prepare(data, mappings, prohibited, options = {})
+        out = apply_maps(data, mappings)
+        normalize(out) if options[:normalize]
+        check_prohibited(out, prohibited)
+        check_bidi(out) if options[:bidi]
+        out
+      end
+
+      private
+
+      def apply_maps(data, mappings)
+        out = ''
+
+        data.each_char do |c|
+          out << mapping(c, mappings)
+        end
+
+        out
+      end
+
+      def check_bidi(out)
+        if out.any? { |c| table_contains?(Tables::C8, c) }
+          raise Error::StringPrep(Error::StringPrep::INVALID_BIDIRECTIONAL)
+        end
+
+        if out.any? { |c| table_contains?(Tables::D1, c) }
+          unless table_contains?(Tables::D1, out.first) && table_contains?(Tables::D1, out.last)
+            raise Error::StringPrep(Error::StringPrep::INVALID_BIDIRECTIONAL)
+          end
+        end
+      end
+
+      def check_prohibited(out, prohibited)
+        out.any? do |c|
+          prohibited.any? do |table|
+            if table_contains?(table, c)
+              raise Error::StringPrep(Error::StringPrep::PROHIBITED_CHARACTER)
+            end
+          end
+        end
+      end
+
+      def mapping(c, mappings)
+        m = mappings.find { |m| m.has_key?(c) }
+        return c if m.nil?
+
+        m[c]
+      end
+
+      def normalize(out)
+        out.unicode_normalize!(:nfkc)
+      end
+
+      def table_contains?(table, c)
+        table.any? do |r|
+          r.member?(c)
+        end
+      end
+    end
+  end
+end
---
+++ lib/mongo/auth/stringprep/profiles/sasl.rb
@@ -0,0 +1,50 @@
+require 'mongo/auth/stringprep/tables'
+
+module Mongo
+  module Auth
+    module StringPrep
+      module Profiles
+        module SASL
+          MAP_NON_ASCII_TO_SPACE = {
+            0x00A0 => [0x0020],
+            0x1680 => [0x0020],
+            0x2000 => [0x0020],
+            0x2001 => [0x0020],
+            0x2002 => [0x0020],
+            0x2003 => [0x0020],
+            0x2004 => [0x0020],
+            0x2005 => [0x0020],
+            0x2006 => [0x0020],
+            0x2007 => [0x0020],
+            0x2008 => [0x0020],
+            0x2009 => [0x0020],
+            0x200A => [0x0020],
+            0x200B => [0x0020],
+            0x202F => [0x0020],
+            0x205F => [0x0020],
+            0x3000 => [0x0020],
+          }.freeze
+
+          MAPPINGS = [
+            Tables::B1,
+            MAP_NON_ASCII_TO_SPACE,
+          ].freeze
+
+          PROHIBITIED = [
+            Tables::A1,
+            Tables::C1_2,
+            Tables::C2_1,
+            Tables::C2_2,
+            Tables::C3,
+            Tables::C4,
+            Tables::C5,
+            Tables::C6,
+            Tables::C7,
+            Tables::C8,
+            Tables::C9,
+          ].freeze
+        end
+      end
+    end
+  end
+end
